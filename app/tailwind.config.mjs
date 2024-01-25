f3ab34a01343017049264e6d0b4c74a800d82ca9
Tomas Emanuel Ramirez Abarca <emanuelrr55@gmail.com>
2024-01-25 00:50:37 -0600

    🐱‍🏍 First Commit

---
+++ app/tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        "rose-white": "hsl(330, 100%, 98%)",
+      },
+      textColor: {
+        "dark-raspberry": "hsl(332, 51%, 32%)",
+        "light-grey": "hsl(30, 18%, 87%)",
+        "wenge-brown": "hsl(30, 10%, 34%)",
+        "dark-charcoal": "hsl(24, 5%, 18%)",
+      },
+    },
+  },
+  plugins: [],
+};
